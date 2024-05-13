7398abee31581f1a7f4ff022110801a23d22721f
Tim Vucina <tim.vucina@gmail.com>
2024-05-13 13:13:05 +0200

    initial project setup

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+// @ts-check
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  tailwindConfig: './tailwind.config.ts',
+  tailwindAttributes: ['className'],
+  tailwindFunctions: ['cn', 'tw', 'clsx'],
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+};
