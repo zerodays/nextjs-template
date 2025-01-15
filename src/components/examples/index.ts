b1ec847538c82872072cc4d154dddf89d9236f0f
Tim Vucina <tim.vucina@gmail.com>
2025-01-15 14:20:47 +0100

    fix: remove zodios examples

--- src/components/examples/index.ts
+++ src/components/examples/index.ts
@@ -1,5 +1,3 @@
 export { default as FormExample } from './form-example';
 export { default as SentryExample } from './sentry-example';
 export { default as ToastExample } from './toast-example';
-export { default as ZodiosClientExample } from './zodios-client-example';
-export { default as ZodiosServerExample } from './zodios-server-example';
