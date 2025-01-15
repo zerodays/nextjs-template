bd235feb39e522c3fd3e93881beb92bfafcd8c38
Tim Vucina <tim.vucina@gmail.com>
2025-01-15 11:28:10 +0100

    fix: update all deps

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,20 +3,21 @@ import { I18nProvider } from '@/i18n/client';
 import { defaultLocale, isLocale, Locale } from '@/i18n/i18n';
 import { ReactElement } from 'react';
 
-export default function Layout({
-  params: { locale: localeString },
+export default async function Layout({
+  params,
   children,
 }: {
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
   children: ReactElement;
 }) {
-  let locale: Locale = defaultLocale;
-  if (isLocale(localeString)) {
-    locale = localeString;
+  let computedLocale: Locale = defaultLocale;
+  const { locale } = await params;
+  if (isLocale(locale)) {
+    computedLocale = locale;
   }
 
   return (
-    <I18nProvider locale={locale}>
+    <I18nProvider locale={computedLocale}>
       {children}
       <Toaster />
     </I18nProvider>
