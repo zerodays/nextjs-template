ce302c062759aab2c1edc5a645a8f68e75660fe9
Vane Skubic <vane.skubic@gmail.com>
2025-10-28 16:38:47 +0100

    fix: used URL for `metadataBase`

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -28,7 +28,7 @@ export async function generateMetadata(): Promise<Metadata> {
         },
       ],
     },
-    metadataBase: env.NEXT_PUBLIC_BASE_URL,
+    metadataBase: new URL(env.NEXT_PUBLIC_BASE_URL ?? ''),
   };
 }
 
