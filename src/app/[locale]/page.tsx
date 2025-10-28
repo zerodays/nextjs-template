0e9e45e53c279e071087fe195e98940ef31efa94
Vane Skubic <vane.skubic@gmail.com>
2025-10-28 13:39:22 +0100

    chore: reverted comment

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -10,7 +10,7 @@ import {
 } from '@/components/examples';
 import ApiExample from '@/components/examples/api-example';
 import ExampleCard from '@/components/examples/example-card';
-// import { BackgroundBeams } from '@/components/ui/background-beams';
+import { BackgroundBeams } from '@/components/ui/background-beams';
 import { getScopedI18n } from '@/i18n/server';
 import { FaGithub } from 'react-icons/fa';
 import readme from '../../../README.md';
@@ -20,7 +20,7 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col items-stretch gap-y-8 py-4">
-      {/* <BackgroundBeams /> */}
+      <BackgroundBeams />
       <Navbar>
         <a
           className="justify-self-center"
