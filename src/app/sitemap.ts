b284387bc33a53832b40d519e3dd53e5ad1a015e
Vane Skubic <vane.skubic@gmail.com>
2025-10-28 15:12:16 +0100

    feat: added sample `robots.ts` and `sitemap.ts`

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import env from '@/env';
+import { LOCALES } from '@/i18n/i18n';
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = env.NEXT_PUBLIC_BASE_URL;
+
+  const routes = ['/']; // Add routes here
+
+  const sitemapEntries: MetadataRoute.Sitemap = [];
+
+  for (const route of routes) {
+    for (const locale of LOCALES) {
+      sitemapEntries.push({
+        url: `${baseUrl}/${locale}${route}`,
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+        priority: route === '/' ? 1 : 0.8,
+      });
+    }
+  }
+
+  return sitemapEntries;
+}
