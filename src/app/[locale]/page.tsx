7c1434bc28d77d1d394ccacc64179607a14cc981
Vane Skubic <vane.skubic@gmail.com>
2025-10-29 09:00:50 +0100

    fix: updated husky `commit-msg` script

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -11,7 +11,7 @@ import {
 } from '@/components/examples';
 import ApiExample from '@/components/examples/api-example';
 import ExampleCard from '@/components/examples/example-card';
-import { BackgroundBeams } from '@/components/ui/background-beams';
+// import { BackgroundBeams } from '@/components/ui/background-beams';
 import { getScopedI18n } from '@/i18n/server';
 import readme from '../../../README.md';
 
@@ -20,7 +20,7 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col items-stretch gap-y-8 py-4">
-      <BackgroundBeams />
+      {/* <BackgroundBeams /> */}
       <Navbar>
         <a
           className="justify-self-center"
